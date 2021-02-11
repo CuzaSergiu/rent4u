7c0d20ac17dfe7e1cb510e18eb299610af617a12
Cuza Sergiu <cuza.sergiu@yahoo.com>
2021-02-11 20:48:02 +0200

    Update RegisterController.java

--- src/main/java/com/project/rent4u/controller/RegisterController.java
+++ src/main/java/com/project/rent4u/controller/RegisterController.java
@@ -1,10 +1,7 @@
 package com.project.rent4u.controller;
 
 import com.project.rent4u.dto.UserDto;
-import com.project.rent4u.service.UserService;
-import com.project.rent4u.service.validator.UserDtoValidator;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,16 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @Slf4j
 public class RegisterController {
-
-    private UserDtoValidator userDtoValidator;
-    private UserService userService;
-
-    @Autowired
-    public RegisterController(UserDtoValidator userDtoValidator, UserService userService) {
-        this.userDtoValidator = userDtoValidator;
-        this.userService = userService;
-    }
-
 
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
@@ -35,14 +22,7 @@ public class RegisterController {
     @PostMapping
     public String postRegisterPage(Model model, UserDto userDto, BindingResult bindingResult) {
         log.info("postRegisterPage called");
-        userDtoValidator.validate(userDto,bindingResult);
-        if (bindingResult.hasErrors()){
-            //todo - cum pot sa pastrez datele dupa ce primesc eroare
-            model.addAttribute("userDto", userDto);
-            return "register";
-        }
-        userService.register(userDto);
-        return "redirect:/register";
+
     }
 
 
