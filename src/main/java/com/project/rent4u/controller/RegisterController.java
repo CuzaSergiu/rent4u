a10c99e4ec92a56560f73f3a556c88c2ccb6c303
Adrian Enache <constantin.adrian.enache@gmail.com>
2021-02-11 20:46:52 +0200

    created postRegisterPage() method

--- src/main/java/com/project/rent4u/controller/RegisterController.java
+++ src/main/java/com/project/rent4u/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package com.project.rent4u.controller;
 
 import com.project.rent4u.dto.UserDto;
+import com.project.rent4u.service.UserService;
+import com.project.rent4u.service.validator.UserDtoValidator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +14,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @Slf4j
 public class RegisterController {
+
+    private UserDtoValidator userDtoValidator;
+    private UserService userService;
+
+    @Autowired
+    public RegisterController(UserDtoValidator userDtoValidator, UserService userService) {
+        this.userDtoValidator = userDtoValidator;
+        this.userService = userService;
+    }
 
 
     @GetMapping("/register")
@@ -23,7 +35,14 @@ public class RegisterController {
     @PostMapping
     public String postRegisterPage(Model model, UserDto userDto, BindingResult bindingResult){
         log.info("postRegisterPage called");
-
+        userDtoValidator.validate(userDto,bindingResult);
+        if (bindingResult.hasErrors()){
+            //todo - cum pot sa pastrez datele dupa ce primesc eroare
+            model.addAttribute("userDto", userDto);
+            return "register";
+        }
+        userService.register(userDto);
+        return "redirect:/register";
     }
 
 
