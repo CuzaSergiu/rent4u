f48da5a4cf92f83f01c5ea27e11b46500d3bed37
Cuza Sergiu <cuza.sergiu@yahoo.com>
2021-02-11 21:43:07 +0200

    role model, repo, UserDetailService and SecurityConfig added

--- src/main/java/com/project/rent4u/controller/RegisterController.java
+++ src/main/java/com/project/rent4u/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package com.project.rent4u.controller;
 
 import com.project.rent4u.dto.UserDto;
+import com.project.rent4u.service.UserService;
+import com.project.rent4u.service.validator.UserDtoValidator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,17 +15,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 public class RegisterController {
 
+    // == fields ==
+    private final UserDtoValidator userDtoValidator;
+    private final UserService userService;
+
+    // == constructor ==
+    @Autowired
+    public RegisterController(UserDtoValidator userDtoValidator, UserService userService) {
+        this.userDtoValidator = userDtoValidator;
+        this.userService = userService;
+    }
+
+    // == methods ==
     @GetMapping("/register")
-    public String getRegisterPage(Model model) {
+    public String getRegisterPage(Model model){
         log.info("getRegisterPage called");
-        model.addAttribute("userDto", new UserDto());
+        model.addAttribute("userDto",new UserDto());
         return "register";
     }
 
     @PostMapping
-    public String postRegisterPage(Model model, UserDto userDto, BindingResult bindingResult) {
+    public String postRegisterPage(Model model, UserDto userDto, BindingResult bindingResult){
         log.info("postRegisterPage called");
-
+        userDtoValidator.validate(userDto,bindingResult);
+        if (bindingResult.hasErrors()){
+            //todo - cum pot sa pastrez datele dupa ce primesc eroare
+            model.addAttribute("userDto", userDto);
+            return "register";
+        }
+        userService.register(userDto);
+        return "redirect:/register";
     }
 
 
